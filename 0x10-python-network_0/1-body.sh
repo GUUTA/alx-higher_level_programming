b4d3dd18fd447beca4a1983c4ff431ae52781bbc
GUUTA <guutatesema@gmail.com>
2022-12-19 08:34:28 -0800

    removed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
-#!/bin/bash                                                                     
-#  takes in a URL, sends a GET request to the URL, and displays the body of the\
- response                                                                       
-curl -sL "$1"
+#!/bin/bash
+# Gets the body of a response from a URL if the status code is 200
+curl -s -L "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,5 @@
+
+
+#!/bin/bash
+# Sends a DELETE HTTP request to a URL
+curl -s -X DELETE "$1"
